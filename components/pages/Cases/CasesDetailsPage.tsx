48576c27e51113a67f24430ac134f16dfbadb4f6
Olha Sotova <sotovaola21@gmail.com>
2026-03-05 23:01:25 +0100

    go

--- components/pages/Cases/CasesDetailsPage.tsx
+++ components/pages/Cases/CasesDetailsPage.tsx
@@ -1,5 +1,5 @@
-import { CaseHero } from "@/components/sections/CaseHero/CaseHero";
-import type { CaseHeroData } from "@/components/sections/CaseHero/types";
+import CaseHero from "@/components/sections/CaseHero/CaseHero";
+// import type { CaseHeroData } from "@/components/sections/CaseHero/types";
 import { CtaBanner } from "@/components/home/CtaBanner/CtaBanner";
 
 type Props = {
@@ -7,34 +7,34 @@ type Props = {
 };
 
 export function CaseDetailsPage({ slug }: Props) {
-  // временно: один кейс. Позже: find по slug или fetch с бэка.
-  const hero: CaseHeroData = {
-    title: "MODERN LOFT",
-    imageSrc: "/images/hero.jpg",
-    imageAlt: "Modern loft",
+  // const hero: CaseHeroData = {
+  //   title: "MODERN LOFT",
+  //   imageSrc: "/images/hero.jpg",
+  //   imageAlt: "Modern loft",
 
-    leftMetrics: [
-      { label: "Бюджет:", value: "$60,000" },
-      { label: "ROI:", value: "35%" },
-      { label: "Метраж:", value: "100" },
-    ],
-    rightMetrics: [
-      { label: "Сроки реализации:", value: "6 мес." },
-      { label: "Локация:", value: "Москва" },
-      { label: "ЖК:", value: "Самолет" },
-    ],
+  //   leftMetrics: [
+  //     { label: "Бюджет:", value: "$60,000" },
+  //     { label: "ROI:", value: "35%" },
+  //     { label: "Метраж:", value: "100" },
+  //   ],
 
-    description:
-      "Ваш путеводитель по лучшим полям для гольфа в Марбелье. Марбелья — это не только пляжи и роскошная жизнь.",
-    ctaLabel: "Забронировать слот",
-    ctaHref: "/packages",
-  };
+  //   rightMetrics: [
+  //     { label: "Сроки реализации:", value: "6 мес." },
+  //     { label: "Локация:", value: "Москва" },
+  //     { label: "ЖК:", value: "Самолет" },
+  //   ],
+
+  //   description:
+  //     "Ваш путеводитель по лучшим полям для гольфа в Марбелье. Марбелья — это не только пляжи и роскошная жизнь.",
+
+  //   ctaLabel: "Забронировать слот",
+  //   ctaHref: "/packages",
+  // };
 
   return (
     <main className="bg-white text-black">
-      <CaseHero data={hero} />
+      {/* <CaseHero data={hero} /> */}
 
-      {/* дальше добавим блоки кейса */}
       <CtaBanner />
     </main>
   );
